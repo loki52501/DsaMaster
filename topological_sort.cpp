366defe0f66fbd85a1c753d046c3f983f58bb70b
loki52501 <artcreativity2019@gmail.com>
2026-03-28 22:19:06 -0400

    mapped dependency using unordered map

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 vector<int> topologicalSort(vector<int> jobs, vector<vector<int>> deps) {
-   unordered_map<int,vector<int>> jobord;
-   
+   unordered_map<int,vector<int>> nextjob;
+   unordered_map<int,int>jobcount;
+   for(auto dep:dep)
+   {
+        if(jobcount[dep[0]].count())
+        jobcount[dep[0]]++;
+        else
+        jobcount[dep[0]]=1;
+        
+        nextjob[dep[0]].push_back(dep[1]);
+    }
+    for(auto orderedjob:nextjob)
+    {
+        
+    }
+
   return {};
 }
