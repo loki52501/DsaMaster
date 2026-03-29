dda7390075bf5e7eab5e9ab72ef3c9f2a49fb26f
loki52501 <artcreativity2019@gmail.com>
2026-03-28 22:21:23 -0400

    did a small mistake in topology sort, instead of counting dependencies , i counted the degree.

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -4,18 +4,18 @@ using namespace std;
 vector<int> topologicalSort(vector<int> jobs, vector<vector<int>> deps) {
    unordered_map<int,vector<int>> nextjob;
    unordered_map<int,int>jobcount;
-   for(auto dep:dep)
+   for(auto dep:deps)
    {
-        if(jobcount[dep[0]].count())
-        jobcount[dep[0]]++;
+        if(jobcount[dep[1]].count())
+        jobcount[dep[1]]++;
         else
-        jobcount[dep[0]]=1;
+        jobcount[dep[1]]=1;
         
         nextjob[dep[0]].push_back(dep[1]);
     }
     for(auto orderedjob:nextjob)
     {
-        
+
     }
 
   return {};
