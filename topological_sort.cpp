308561c0a0c9e30f02960ab80aec8965df6fb8b4
loki52501 <artcreativity2019@gmail.com>
2026-03-28 22:33:37 -0400

    while ordering or scheduling the job, i'm taking the job with zero dependency as first to be executed and push jobs that are depenedent on these jobs into the queue.

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -19,7 +19,16 @@ vector<int> topologicalSort(vector<int> jobs, vector<vector<int>> deps) {
     }
     for(auto job:jobs)
     { 
-      order.
+        if(jobcount[job]==0)
+        order.push(job);
+
+    }
+    while(!order.empty())
+    {
+      res.push(order.front());
+      order.pop();
+      for(auto nextb:nextjob[job])
+      queue.push(nextb);
     }
 
   return {};
